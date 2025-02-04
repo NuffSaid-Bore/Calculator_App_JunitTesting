7b32c0d093cfa7b4f4e670fe51f1434d0e4202bb
NuffSaid-Bore <borekamohelo@gmail.com>
2025-02-04 13:58:48 +0200

    modified Model class, repository class

--- src/main/java/com/java/data/UserRepository.java
+++ src/main/java/com/java/data/UserRepository.java
@@ -4,14 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.java.Models.User;
+import com.java.Models.User.UserType;
 
 public class UserRepository {
 
     private Map<String , User> users = new HashMap<String, User>();
 
     public UserRepository(){
-        users.put("matt",new User("matt", "letmein"));
-        users.put("frank",new User("frank", "myPassword"));
+        users.put("matt",new User("matt", "letmein",false, UserType.REGULAR_USER ));
+        users.put("frank",new User("frank", "myPassword", false, UserType.ADMIN_USER));
     }
 
     public User findByUsername(String username) {
